c592096164d29f960dc4262794ed31a39b3e0218
junghyunl <83959932+junghyunl@users.noreply.github.com>
2021-05-17 18:37:53 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+//alert('Im working. Im JS. Im Beautiful. Im worth it');
+
+/*
+let a = 500;
+let b = a - 100;
+a = 39;
+console.log(b, a);
+
+const what = "junghyun";
+
+const wat = true;
+
+const wat = 666;
+
+const wat = 40.5; 
+
+
+const something = "Something";
+
+const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", 990, false, "Sun", something];
+
+console.log(daysOfWeek);
+*/
