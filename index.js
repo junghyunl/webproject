72c6e056eb6272d8460c2e54055b41af6a8e4664
junghyunl <83959932+junghyunl@users.noreply.github.com>
2021-05-24 20:59:46 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -20,4 +20,24 @@ const something = "Something";
 const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", 990, false, "Sun", something];
 
 console.log(daysOfWeek);
-*/
+
+const jhInfo = {
+    name: "Junghyun",
+    gender: "Female",
+    major: "computer science"
+}
+
+console.log(jhInfo.console)
+
+function sayHello(a, b) {
+    console.log('Hello!', a, "I have", b);
+}
+
+sayHello("Junghyun", 100);
+console.log("Hi!")*/
+
+function sayHello(a, b) {
+    console.log(`Hello ${a} you are ${b} years old`);
+}
+
+sayHello("Junghyun", 100);
