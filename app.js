f3865e76e7c2b3e7b911fc05f9af1f12849a153a
junghyunl <83959932+junghyunl@users.noreply.github.com>
2021-06-29 20:59:52 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -8,11 +8,18 @@ const loginInput = document.querySelector("#login-form input");
 /*
 const loginButton = document.querySelector("#login-form button");
  */
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    //const username = loginInput.value;
-    console.log(loginInput.value);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+    //console.log(loginInput.value);
     /*if (username === "") {
         alert("Please write your name");
     } else if (username.length > 15) {
@@ -20,5 +27,19 @@ function onLoginSubmit(event) {
     }*/
 }
 
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+//정보 저장, 삭제
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListner("submit", onLoginSubmit);
+}
+else {
+    paintGreetings(savedUsername);
+}
+
 //loginButton.addEventListener("click", onLoginBtnClick);
-loginForm.addEventListner("submit", onLoginSubmit);
